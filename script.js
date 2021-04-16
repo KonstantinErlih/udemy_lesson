f7a5abf38de040680da9d53ca5894fee8d96b290
Erlih <erlih@bk.ru>
2021-04-16 20:05:53 +0300

    Start commit

---
+++ script.js
@@ -0,0 +1,4 @@
+let user = 'John Lennon';
+let category = 'point';
+console.log(`Привет, ${user}`); // Привет, John Lennon
+console.log(`http://someurl.com/${category}`); // http://someurl.com/point
