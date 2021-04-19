55c8fcc42ba685efe8e09782522982fa0790c777
Erlih <erlih@bk.ru>
2021-04-19 10:32:02 +0300

    lesson17

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-let user = 'John Lennon';
-let category = 'point';
-console.log(`Привет, ${user}`); // Привет, John Lennon
-console.log(`http://someurl.com/${category}`); // http://someurl.com/point
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+for (let i = 1; i <= 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его', '');
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+        console.log('error');
+    }
+}
+
+console.log(personalMovieDB);
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (numberOfFilms >= 10 && numberOfFilms < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
